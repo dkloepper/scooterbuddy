c0a01ed020d8f86438c402968f7b836079db3e51
dkloepper <kloe0021@umn.edu>
2022-12-13 20:48:31 -0600

    Update 2_Data_Overview.py
    Fixing links

--- pages/2_Data_Overview.py
+++ pages/2_Data_Overview.py
@@ -4,10 +4,10 @@ st.title("Scooter Buddy -- Data Overview")
 st.markdown("""---""")
 
 st.header("Data Source --  Open Data Minneapolis")
-st.markdown('<a url="https://opendata.minneapolismn.gov/">Open Data Minneapolis</a>',unsafe_allow_html=True)
+st.markdown('<a href="https://opendata.minneapolismn.gov/">Open Data Minneapolis</a>',unsafe_allow_html=True)
 st.markdown("The city of Minneapolis provides public access to a variety of GIS and non-spatial data, including things like 311 activity, snow emergency towing, and even scooters.")
 st.subheader("Scooter Data")
-st.markdown('<a url="https://opendata.minneapolismn.gov/search?groupIds=9bc71a032e984a22a5e94312d9d9bf7f">Scooter Data</a>',unsafe_allow_html=True)
+st.markdown('<a href="https://opendata.minneapolismn.gov/search?groupIds=9bc71a032e984a22a5e94312d9d9bf7f">Scooter Data</a>',unsafe_allow_html=True)
 st.subheader("Location Data")
 
 st.header("Data Type -- GeoJSON")
