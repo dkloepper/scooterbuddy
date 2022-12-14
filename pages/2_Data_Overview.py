705cfaf29a3c74915d7dcc24340a6517ccab8d1c
dkloepper <kloe0021@umn.edu>
2022-12-13 22:07:58 -0600

    Update 2_Data_Overview.py
    Lots of text updates

--- pages/2_Data_Overview.py
+++ pages/2_Data_Overview.py
@@ -19,10 +19,23 @@ st.markdown('- Neighborhood: The neighborhood the centerline segment is located 
 
 st.subheader("Location Data")
 st.markdown('I used two location-based datasets as input and connection to the scooter data:')
-st.markdown('- *EAS Address:* This dataset contains all addresses for the city of Minneapolis. ')
-st.markdown('- *PollTime:* The time the API was polled for a snapshot of available scooters.')
+st.markdown('- *EAS Address:* This dataset contains all addresses for the city of Minneapolis. From this dataset, I used the "display" address as well as the latitude/longitude of the address.')
+st.markdown('<a href="https://opendata.minneapolismn.gov/datasets/cityoflakes::eas-addresses/about">EAS Addresses/a>',unsafe_allow_html=True)
+st.markdown('- *MPLS:* This dataset contains the coordinates for the center of the pavement for all drivable streets in Minneapolis. These centerlines are used as locations in the scooter data')
+st.markdown('<a href="https://opendata.minneapolismn.gov/datasets/cityoflakes::mpls-centerline/about">MPLS Centerline</a>',unsafe_allow_html=True)
 
+st.header("Data Type -- GeoJSON & GeoPandas")
+st.markdown('''GeoJSON is a format for encoding geographic structures like points, lines, and various polygon shapes.
 
-st.header("Data Type -- GeoJSON")
+In the case of the address data, each record contained a POINT with the latitude/longitude coordinates.
+
+The centerline data was, unsuprisinly a LineString with begining and end points marked with latitude/longitude.''')
+st.markdown('<a href="https://geojson.org/">GeoPandas</a>',unsafe_allow_html=True)
+
+st.markdown('''The GeoPandas module extends the standard features of Pandas, making it easy to work with the GeoJSON data types within a dataframe. 
+It also provides convient functions for processes like calculating distance or finding the centroid of two points''')
+st.markdown('<a href="https://geopandas.org/en/stable/">GeoPandas</a>',unsafe_allow_html=True)
 
 st.header("Data Cleansing")
+st.subheader("Scooter Data")
+st.subheader("Location Data")
